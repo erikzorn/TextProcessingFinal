45ece8e24cc4f15c16c8a212554cdf0f1f83b704
ElyYonce <elyyonce11@gmail.com>
2019-04-20 18:35:54 -0400

    generated value counts for each article

--- processing.py
+++ processing.py
@@ -1,7 +1,8 @@
-#CSCE 578 Final Project - Ely Yonce & Ely Yonce
+#CSCE 578 Final Project - Ely Yonce & Erik Zorn
 #In this project we will be doing cluster analysis on a dataset of fake news aricles
 #We want to find common themes in the fake news based on term frequency
 ###########################################################################
+#Get term frequency from each one of the articles
 #import needed libraries
 import pandas as pd
 import numpy as np
@@ -10,3 +11,11 @@ import numpy as np
 fakeNewsDF = pd.read_csv('./fake.csv')
 #split the text on white space for each one of the articles
 splitArticles = fakeNewsDF['text'].str.split(' ')
+#init dictionary to hold tf vals
+termsDict = {}
+#loop through all of the articles
+index = 0
+for article in splitArticles:
+    articleSeries = pd.Series(article)
+    termsDict[index] = articleSeries.value_counts()
+    index += 1
